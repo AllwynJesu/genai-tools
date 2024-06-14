66132b6d0f4d1fffd15e01959ffbc1c859f56ac8
Allwyn Jesu <allwynit@gmail.com>
2024-06-14 13:14:04 +0530

    Removed content.js

--- content.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message === "get-selection") {
-    const selectedText = window.getSelection().toString().trim();
-    console.log(selectedText)
-    sendResponse(selectedText);
-  }
-});
